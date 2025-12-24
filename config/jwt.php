50da4f1368ec520608b64fa6e0f1ab261eceaf99
北有先生 <chuanbei32@qq.com>
2025-12-24 23:10:05 +0800

    ..

--- config/jwt.php
+++ config/jwt.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'secret'      => env('JWT_SECRET', 'your_secret_key'),
+    'secret'      => env('JWT_SECRET', 'f4d8e2c9a1b3c5e7d9f0a2b4c6e8d0f1a3b5c7e9d1f2a4b6c8e0d2f3a5b7c9e1'),
     //  有效期 (秒) 默认 1小时
     'ttl'         => env('JWT_TTL', 3600),
     //  刷新有效期 (秒) 默认 2周
