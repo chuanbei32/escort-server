6cc7597cd7248a88f4550b96893e22fcc01f1244
北有先生 <chuanbei32@qq.com>
2025-12-30 20:23:17 +0800

    ..

--- app/admin/controller/Service.php
+++ app/admin/controller/Service.php
@@ -6,6 +6,7 @@ use app\common\controller\AdminController;
 use app\admin\service\annotation\ControllerAnnotation;
 use app\admin\service\annotation\NodeAnnotation;
 use think\App;
+use app\Request;
 
 #[ControllerAnnotation(title: 'service')]
 class Service extends AdminController
@@ -23,6 +24,22 @@ class Service extends AdminController
         $this->assign(compact('notes'));
     }
 
+    #[NodeAnnotation(title: '删除', auth: true)]
+    public function delete(Request $request): void
+    {
+        // 如果不是id作为主键 请在对应的控制器中覆盖重写
+        $id = $request->param('id', []);
+
+        $this->checkPostRequest();
+        $row = self::$model::where('id', 'not in', [1])->whereIn('id', $id)->select();
+        $row->isEmpty() && $this->error('数据不存在');
+        try {
+            $save = $row->delete();
+        }catch (\Exception $e) {
+            $this->error('删除失败');
+        }
+        $save ? $this->success('删除成功') : $this->error('删除失败');
+    }
     
 
 }
