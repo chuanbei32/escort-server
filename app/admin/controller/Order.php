587fb028f98c443f679a42b6304b7f1de158ebf5
北有先生 <chuanbei32@qq.com>
2026-01-04 21:11:47 +0800

    ..

--- app/admin/controller/Order.php
+++ app/admin/controller/Order.php
@@ -6,6 +6,11 @@ use app\common\controller\AdminController;
 use app\admin\service\annotation\ControllerAnnotation;
 use app\admin\service\annotation\NodeAnnotation;
 use think\App;
+use think\response\Json;
+use app\Request;
+use app\admin\model\User;
+use app\admin\model\Service;
+
 
 #[ControllerAnnotation(title: 'order')]
 class Order extends AdminController
@@ -23,6 +28,58 @@ class Order extends AdminController
         $this->assign(compact('notes'));
     }
 
-    
+    #[NodeAnnotation(title: '列表', auth: true)]
+    public function index(Request $request): Json|string
+    {
+        if ($request->isAjax()) {
+            if (input('selectFields')) {
+                return $this->selectList();
+            }
+            list($page, $limit, $where) = $this->buildTableParams();
 
+            $newWhere = [];
+            foreach ($where as $key => $value) {
+                switch ($value[0]) {
+                    case 'order_sn':
+                        $newWhere['order_sn'] = $value[2];
+                        break;
+                    case 'status':
+                        $newWhere['status'] = $value[2];
+                        break;
+                    case 'user_nickname':
+                        $newWhere['user_id'] = User::where('nickname', 'like', "%{$value[2]}%")->value('id');
+                        break;
+                    case 'service_name':
+                        $newWhere['service_id'] = Service::where('name', 'like', "%{$value[2]}%")->value('id');
+                        break;
+                }
+            }
+            $count = self::$model::with(['user', 'service', 'userCoupon'])->where($newWhere)->count();
+            $list  = self::$model::with(['user', 'service', 'userCoupon'])->withAttr('is_coupon')->where($newWhere)->page($page, $limit)->order($this->sort)->select()->toArray();
+            $data  = [
+                'code'  => 0,
+                'msg'   => '',
+                'count' => $count,
+                'data'  => $list,
+            ];
+            return json($data);
+        }
+        return $this->fetch();
+    }
+
+    #[NodeAnnotation(title: '退款', auth: true)]
+    public function refund(Request $request): Json|string
+    {
+        $id = $request->param('id', []);
+        $this->checkPostRequest();
+        $row = self::$model::whereIn('id', $id)->select();
+        $row->isEmpty() && $this->error('数据不存在');
+        try {
+            // 调用支付宝退款接口
+            $save = $row->refund();
+        }catch (\Exception $e) {
+            $this->error('退款失败');
+        }
+        $save ? $this->success('退款成功') : $this->error('退款失败');
+    }
 }
--- app/admin/model/Order.php
+++ app/admin/model/Order.php
@@ -17,7 +17,37 @@ class Order extends TimeModel
     }
 
     public static array $notes = [
+      'is_coupon' => [
+        0 => '否',
+        1 => '是',
+      ],
+      'status' => [
+    0 => '待支付',
+    1 => '已支付',
+    2 => '已退款',
+    3 => '已完成',
+  ],
 ];
+
+  protected function getIsCouponAttr($value, $data): string
+    {
+        return $data['coupon_id'] != 0 ? 1 : 0;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class, 'service_id', 'id');
+    }
+
+    public function userCoupon()
+    {
+        return $this->belongsTo(UserCoupon::class, 'coupon_id', 'id');
+    }
 
     
 
