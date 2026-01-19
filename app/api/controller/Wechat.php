fba64d1fdb3a74fe5c16f413e3b0da4318b69dd6
北有先生 <chuanbei32@qq.com>
2026-01-19 11:07:19 +0800

    ..

--- app/api/controller/Wechat.php
+++ app/api/controller/Wechat.php
@@ -16,13 +16,6 @@ class Wechat extends Base
      */
     public function server()
     {
-        $config = Config::get('wechat.mini_program');
-        if (empty($config['app_id'])) {
-            Log::error('微信小程序配置缺失');
-            return response('Configuration missing', 400);
-        }
-
-        $app = new Application($config);
         $thinkRequest = request();
         
         // 获取请求原始内容
@@ -34,10 +27,30 @@ class Wechat extends Base
         Log::debug('请求头：' . json_encode($thinkRequest->header(), JSON_UNESCAPED_UNICODE));
         Log::debug('请求内容：' . $content);
 
+        $config = Config::get('wechat.mini_program');
+        if (empty($config['app_id'])) {
+            Log::error('微信小程序配置缺失');
+            return response('Configuration missing', 400);
+        }
+
+        $app = new Application($config);
+
         // 如果是 GET 请求且包含 echostr，通常是微信服务器的 URL 验证
         if ($thinkRequest->isGet() && $thinkRequest->has('echostr')) {
             Log::info('处理微信服务器 URL 验证');
-            return response($thinkRequest->get('echostr'));
+            // 创建 Symfony Request 对象供 EasyWeChat 使用
+            $symfonyRequest = new SymfonyRequest(
+                $_GET,
+                $_POST,
+                [],
+                $_COOKIE,
+                $_FILES,
+                $_SERVER,
+                $content
+            );
+            $app->setRequestFromSymfonyRequest($symfonyRequest);
+            $server = $app->getServer();
+            return $server->serve();
         }
 
         // 如果是 POST 请求但内容为空，EasyWeChat 会抛出异常
@@ -45,19 +58,6 @@ class Wechat extends Base
             Log::warning('收到空的 POST 请求内容');
             return response('success'); // 返回 success 避免微信重试
         }
-
-        // 创建 Symfony Request 对象供 EasyWeChat 使用
-        $symfonyRequest = new SymfonyRequest(
-            $_GET,
-            $_POST,
-            [],
-            $_COOKIE,
-            $_FILES,
-            $_SERVER,
-            $content
-        );
-
-        $app->setRequestFromSymfonyRequest($symfonyRequest);
         $server = $app->getServer();
 
         // 处理小程序消息和事件
